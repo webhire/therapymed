b7030684eb6d9f525dbc6459832395a8ff0d4a17
Erkinov <erkinovit@gmail.com>
2022-08-19 11:51:33 +0500

    Laravel
    Laravel framework was chosen as main technology for developing Therapy Med web application. Current Laravel version is 9.25, PHP version is 8.1

---
+++ webapp/vite.config.js
@@ -0,0 +1,11 @@
+import { defineConfig } from 'vite';
+import laravel from 'webapp-vite-plugin';
+
+export default defineConfig({
+    plugins: [
+        laravel({
+            input: ['resources/css/app.css', 'resources/js/app.js'],
+            refresh: true,
+        }),
+    ],
+});
