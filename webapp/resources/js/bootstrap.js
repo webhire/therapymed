2cdbbc134ac8ceb1ff944577b46a9692eddd5fcc
Erkinov <erkinovit@gmail.com>
2022-08-22 12:59:46 +0500

    TherapyMed-v0.0.1

--- webapp/resources/js/bootstrap.js
+++ webapp/resources/js/bootstrap.js
@@ -18,7 +18,7 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
  * allows your team to easily build robust real-time web applications.
  */
 
-// import Echo from 'webapp-echo';
+// import Echo from 'laravel-echo';
 
 // import Pusher from 'pusher-js';
 // window.Pusher = Pusher;
